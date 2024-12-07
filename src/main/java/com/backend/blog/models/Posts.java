9ad29514b02885f00e5cb1fc8a62dede034949df
JLopes-dev <joaopedrolopessardinha@gmail.com>
2024-12-07 01:44:56 -0300

    add table posts

---
+++ src/main/java/com/backend/blog/models/Posts.java
@@ -0,0 +1,30 @@
+package com.backend.blog.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
