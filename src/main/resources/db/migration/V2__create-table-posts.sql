5a458ed00e3b88d8e1d8af32589ebf0d066e5bf9
JLopes-dev <joaopedrolopessardinha@gmail.com>
2024-12-07 23:29:01 -0300

    add app.properties and docker configuration

--- src/main/resources/db/migration/V2__create-table-posts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE posts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
