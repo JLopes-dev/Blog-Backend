d8938047377cae9fcd5f2f120f631ab5e94b4331
JLopes-dev <joaopedrolopessardinha@gmail.com>
2024-12-14 06:46:36 -0300

    add entity comments

---
+++ src/main/java/com/backend/blog/models/Comment.java
@@ -0,0 +1,31 @@
+package com.backend.blog.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "comments")
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    private String description;
+
+}
